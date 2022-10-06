14a7fa2ce54e27c41b7d304c0cee45a4f3353c73
Den <denis-s2@yandex.ru>
2022-10-06 09:10:30 +0500

    completed his homework

---
+++ Task_41/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+Clear();
+
+// 1. Пользователь вводит с клавиатуры M чисел. 
+// 2. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+WriteLine("Введите числа:");
+string[] nums = ReadLine()!.Split(new char[] { ' ', ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+int[] intArray = Array.ConvertAll(nums, int.Parse);
+WriteLine();
+
+int result = CountNumbers(intArray);
+WriteLine($"Количество чисел больше ноля:\n{result}");
+
+int CountNumbers(int[] intArray)
+{
+    int counter = 0;
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        if (intArray[i] > 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+WriteLine("Введите значения:");
+string[] nums = ReadLine()!.Split(new char[] { ' ', ',', ';', ':' }, StringSplitOptions.RemoveEmptyEntries);
+WriteLine();
+
+GetPoint(double.Parse(nums[0]), double.Parse(nums[1]), double.Parse(nums[2]), double.Parse(nums[3]));
+
+void GetPoint(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * x + b2;
+    if (k1 == k2) System.Console.WriteLine("Параллельные прямые");
+    else System.Console.WriteLine($"Точка пересечения: ({x:f1}, {y:f1})");
+}
